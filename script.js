7f7ecbd804295227b743f4dd883aef047b16530d
Asif-Arafat_Rafat <asif-arafat-rafat@gmail.com>
2024-02-10 14:21:53 +0600

    thats it

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function seeMore(button) {
     const content = button.parentNode.querySelector('span');
     content.style.display = content.style.display === 'none' ? 'inline' : 'none';   
-    button.innerHTML = button.innerHTML === 'See More....' ? 'See Less' : 'See More....';
+}
+function addNews(this) {
+    const content = this.parentElement.querySelector('span');
+    content.style.display = content.style.display === 'none' ? 'inline' : 'none';   
 }
