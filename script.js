9a9235da1b1a24f56717c38387a4d69a91244c2a
Asif-Arafat_Rafat <asif-arafat-rafat@gmail.com>
2024-02-11 03:31:32 +0600

    Done at 3:30am

--- script.js
+++ script.js
@@ -2,33 +2,22 @@ function seeMore(button) {
     const content = button.parentNode.querySelector('span');
     content.style.display = content.style.display === 'none' ? 'inline' : 'none';   
 }
-    const d = new Date();
-    let text = d.toLocaleTimeString();
-    document.querySelector(".time").innerHTML = text;
+   
     document.querySelector(".date").innerHTML =new Date().toLocaleDateString();
 
-
-function addNews() {
-    let newNews=document.querySelector(".allnews")
-    newNews+=`
-    <div class="news">
-    <h1></h1>
-    <h4 class="snippet">
-        
-    </h4>
-    <div class="minews">
-      <p>
-
-        <span>
-        </span>
-        <button class="morebtn" onclick="seeMore(this)">View</button>
-      </p>
-      <img
-        src="https://hackster.imgix.net/uploads/attachments/1682669/robotinlab-scaled_VBqz2nneDV.jpg?auto=compress%2Cformat&w=830&h=466.875&fit=min&dpr=1"
-        alt=""
-      />
-    </div>
-  </div>
-    
-    `
-}
+    function startTime() {
+        const today = new Date();
+        let h = today.getHours();
+        let m = today.getMinutes();
+        let s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        text=h + ":" + m + ":" + s;
+        document.querySelector(".time").innerHTML = text;
+        setTimeout(startTime, 1000);
+      }
+      
+      function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+      }
